058648a251e9d6b42c5776ac3cbbae7fa00acd9a
Kevin Ndung'u <kevgathuku@users.noreply.github.com>
2018-05-20 11:09:29 +0300

    Remove hello_react javascript pack

--- app/javascript/packs/hello-world-bundle.js
+++ app/javascript/packs/hello-world-bundle.js
@@ -1,3 +1,6 @@
+// Run this example by adding <%= javascript_pack_tag 'hello_world-bundle' %> to the head of your layout file,
+// like app/views/layouts/application.html.erb.
+
 import ReactOnRails from 'react-on-rails';
 
 import HelloWorld from '../bundles/HelloWorld/components/HelloWorld';
