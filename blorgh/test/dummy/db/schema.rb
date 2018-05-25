04f2e2719a1de20dc6faf251f6853288fc5b0fae
Kevin Ndung'u <kevgathuku@users.noreply.github.com>
2018-05-25 13:05:16 +0300

    Copy blog migrations to blorgh dummy app

--- blorgh/test/dummy/db/schema.rb
+++ blorgh/test/dummy/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_05_25_095332) do
+ActiveRecord::Schema.define(version: 2018_05_25_100418) do
 
   create_table "blorgh_articles", force: :cascade do |t|
     t.string "title"
