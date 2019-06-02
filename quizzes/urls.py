27085cbf724116fe14388f62041c83a7acbfd974
Kevin Ndung'u <kevgathuku@gmail.com>
2019-06-02 13:55:54 +0300

    Add placeholder views and corresponding urls

--- quizzes/urls.py
+++ quizzes/urls.py
@@ -3,5 +3,12 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+  # ex: /quizzes/
     path('', views.index, name='index'),
+    # ex: /quizzes/5/
+    path('<int:question_id>/', views.detail, name='detail'),
+    # ex: /quizzes/5/results/
+    path('<int:question_id>/results/', views.results, name='results'),
+    # ex: /quizzes/5/vote/
+    path('<int:question_id>/vote/', views.vote, name='vote'),
 ]
