0248c373bb704e020e0b4677169ab914937f30af
Kevin Ndung'u <kevgathuku@gmail.com>
2019-06-02 14:56:22 +0300

    Add additional tests for questions visibility

--- quizzes/views.py
+++ quizzes/views.py
@@ -1,6 +1,7 @@
 from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import get_object_or_404, render
 from django.urls import reverse
+from django.utils import timezone
 from django.views import generic
 
 from .models import Choice, Question
@@ -8,11 +9,16 @@ from .models import Choice, Question
 
 class IndexView(generic.ListView):
     template_name = "quizzes/index.html"
-    context_object_name = 'latest_question_list'
+    context_object_name = "latest_question_list"
 
     def get_queryset(self):
-        """Return the last five published questions."""
-        return Question.objects.order_by('-pub_date')[:5]
+        """
+        Return the last five published questions (not including those set to be
+        published in the future).
+        """
+        return Question.objects.filter(pub_date__lte=timezone.now()).order_by(
+            "-pub_date"
+        )[:5]
 
 
 class DetailView(generic.DetailView):
@@ -22,7 +28,7 @@ class DetailView(generic.DetailView):
 
 class ResultsView(generic.DetailView):
     model = Question
-    template_name = 'quizzes/results.html'
+    template_name = "quizzes/results.html"
 
 
 def vote(request, question_id):
@@ -42,5 +48,5 @@ def vote(request, question_id):
         # Always return an HttpResponseRedirect after successfully dealing
         # with POST data. This prevents data from being posted twice if a
         # user hits the Back button.
-        return HttpResponseRedirect(reverse('quizzes:results', args=(question.id,)))
+        return HttpResponseRedirect(reverse("quizzes:results", args=(question.id,)))
 
