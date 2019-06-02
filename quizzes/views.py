fecc0a7324bb8ae6318b1aa4f90377530b7434bf
Kevin Ndung'u <kevgathuku@gmail.com>
2019-06-02 16:35:25 +0300

    Display all quizzes in the quizzes index route

--- quizzes/views.py
+++ quizzes/views.py
@@ -4,19 +4,19 @@ from django.urls import reverse
 from django.utils import timezone
 from django.views import generic
 
-from .models import Choice, Question
+from .models import Choice, Question, Quiz
 
 
 class IndexView(generic.ListView):
     template_name = "quizzes/index.html"
-    context_object_name = "latest_question_list"
+    context_object_name = "latest_quiz_list"
 
     def get_queryset(self):
         """
         Return the last five published questions (not including those set to be
         published in the future).
         """
-        return Question.objects.filter(pub_date__lte=timezone.now()).order_by(
+        return Quiz.objects.filter(pub_date__lte=timezone.now()).order_by(
             "-pub_date"
         )[:5]
 
