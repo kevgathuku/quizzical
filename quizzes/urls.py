cf48d5692b128eb075f9ba249ecbf96333e02224
Kevin Ndung'u <kevgathuku@gmail.com>
2019-06-02 14:42:32 +0300

    Refactor to class-based views

--- quizzes/urls.py
+++ quizzes/urls.py
@@ -5,11 +5,11 @@ from . import views
 app_name = 'quizzes'
 urlpatterns = [
   # ex: /quizzes/
-    path('', views.index, name='index'),
+    path('', views.IndexView.as_view(), name='index'),
     # ex: /quizzes/5/
-    path('<int:question_id>/', views.detail, name='detail'),
+    path('<int:pk>/', views.DetailView.as_view(), name='detail'),
     # ex: /quizzes/5/results/
-    path('<int:question_id>/results/', views.results, name='results'),
+    path('<int:pk>/results/', views.ResultsView.as_view(), name='results'),
     # ex: /quizzes/5/vote/
     path('<int:question_id>/vote/', views.vote, name='vote'),
 ]
