8fc89a848714f778b9418f159d758b6ad62004cf
Kevin Ndung'u <kevgathuku@gmail.com>
2019-06-02 16:26:05 +0300

    Add Quiz and Answer models to admin

--- quizzes/admin.py
+++ quizzes/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Question, Choice
+from .models import Answer, Quiz, Question, Choice
 
 
 class ChoiceInline(admin.TabularInline):
@@ -10,19 +10,26 @@ class ChoiceInline(admin.TabularInline):
 
 class QuestionAdmin(admin.ModelAdmin):
     fieldsets = [
-        (None,               {'fields': ['question_text']}),
-        ('Date information', {'fields': ['pub_date']}),
+        (None, {"fields": ["question_text"]}),
+        ("Date information", {"fields": ["pub_date"]}),
     ]
     inlines = [ChoiceInline]
-    list_display = ('question_text', 'pub_date', 'was_published_recently')
-    list_filter = ['pub_date']
-    search_fields = ['question_text']
+    list_display = ("question_text", "pub_date", "was_published_recently")
+    list_filter = ["pub_date"]
+    search_fields = ["question_text"]
+
 
 class ChoiceAdmin(admin.ModelAdmin):
-    list_display = ('choice_text', 'votes',)
-    list_filter = ['question']
-    search_fields = ['choice_text']
+    list_display = ("choice_text", "votes")
+    list_filter = ["question"]
+    search_fields = ["choice_text"]
+
+
+class AnswerAdmin(admin.ModelAdmin):
+    list_display = ("question", "correct_choice")
 
 
 admin.site.register(Question, QuestionAdmin)
 admin.site.register(Choice, ChoiceAdmin)
+admin.site.register(Quiz)
+admin.site.register(Answer, AnswerAdmin)
