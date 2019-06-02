059d04998a257b8896619629365ff8958d2c0dbf
Kevin Ndung'u <kevgathuku@gmail.com>
2019-06-02 13:42:06 +0300

    Install django extensions
    Configure the `shell_plus` management command to use ipython

--- quizzical/settings.py
+++ quizzical/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django_extensions',
 ]
 
 MIDDLEWARE = [
@@ -119,3 +120,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# Django extensions
+# Always use IPython for shell_plus
+SHELL_PLUS = "ipython"
